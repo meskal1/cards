7e103dd4b81c8918a6b669e391958f51994abb5a
sanyalu <sanyalu97@gmail.com>
2022-12-12 17:36:58 +0100

    feat(auth): add formik + yup-validation to registration form

--- src/features/auth/Registration/Registration.tsx
+++ src/features/auth/Registration/Registration.tsx
@@ -1,49 +1,68 @@
 import React from 'react'
 
 import { TextField } from '@mui/material'
-import { NavLink } from 'react-router-dom'
+import { useFormik } from 'formik'
+import { Link } from 'react-router-dom'
 
 import { CustomButton } from '../../../common/components/CustomButton/CustomButton'
 import { CustomPasswordInput } from '../../../common/components/CustomPasswordInput/CustomPasswordInput'
 import { PATH } from '../../../constants/routePaths.enum'
+import { validationSchemaRegistration } from '../../../utils/validationSchema'
 
 import s from './Registration.module.scss'
 
 type RegistrationType = {}
 
 export const Registration: React.FC<RegistrationType> = ({}) => {
+  const { handleSubmit, getFieldProps, errors, touched } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: validationSchemaRegistration,
+    onSubmit: values => {
+      console.log(values)
+    },
+  })
+
   return (
     <div className={s.registration}>
-      <form className={s.form}>
+      <form className={s.form} onSubmit={handleSubmit}>
         <h3 className={s.title}>sign up</h3>
         <TextField
           className={s.input}
-          name={'email'}
           label={'Email'}
           variant={'standard'}
-          type={'email'}
-          value={'qwerty@gmail.com'}
           fullWidth
+          autoComplete={'new-password'}
+          error={!!errors.email && touched.email}
+          helperText={touched.email && errors.email}
+          {...getFieldProps('email')}
         />
         <CustomPasswordInput
           className={s.input}
-          name={'password'}
           label={'Password'}
-          value={'qwerty'}
           fullWidth
+          autoComplete={'new-password'}
+          error={!!errors.password && touched.password}
+          helperText={touched.password && errors.password}
+          {...getFieldProps('password')}
         />
         <CustomPasswordInput
           className={s.input}
-          name={'confirmPassword'}
           label={'Confirm password'}
-          value={'qwerty'}
           fullWidth
+          autoComplete={'new-password'}
+          error={!!errors.confirmPassword && touched.confirmPassword}
+          helperText={touched.confirmPassword && errors.confirmPassword}
+          {...getFieldProps('confirmPassword')}
         />
         <CustomButton className={s.button}>sign up</CustomButton>
         <p className={s.signInQuestion}>Already have an account?</p>
-        <NavLink className={s.signInLink} to={PATH.LOGIN}>
+        <Link className={s.signInLink} to={PATH.LOGIN}>
           sign in
-        </NavLink>
+        </Link>
       </form>
     </div>
   )
