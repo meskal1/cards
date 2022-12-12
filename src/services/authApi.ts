f4658ba1319fb96ba21b3f31cdd6fa08340c86f4
sanyalu <sanyalu97@gmail.com>
2022-12-12 23:37:11 +0100

    feat(services): add register api

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 
 // API
 const instance = axios.create({
-  //   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+  baseURL: 'http://localhost:7542/2.0/',
   withCredentials: true,
 })
 
@@ -16,10 +16,44 @@ export const authAPI = {
   me() {
     return instance.get<ResponseType>('')
   },
+  register(data: RegisterParamsType) {
+    return instance.post<RegisterResponseType>('/auth/register', data)
+  },
 }
 
 // TYPES
-export type LoginParamsType = {}
+export type LoginParamsType = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
+
+export type UserType = {
+  _id: string
+  email: string
+  rememberMe: boolean
+  name: string
+  publicCardPacksCount: number
+  created: string
+  updated: string
+  avatar: string
+}
+
+export type RegisterParamsType = Omit<LoginParamsType, 'rememberMe'>
+
+export type RegisterResponseType = RegisterSuccessResponseType
+
+export type RegisterSuccessResponseType = {
+  addedUser: AddedUserType
+}
+
+export type RegisterFailResponseType = {
+  error: string
+  email: string
+  in: string
+}
+
+export type AddedUserType = Omit<UserType, 'avatar'>
 
 export type AuthMeType = {}
 
