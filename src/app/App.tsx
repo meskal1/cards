e31dfd16208296b2b462595c348c068b8496c77a
Daniil <meskal1@rambler.ru>
2022-12-13 21:59:05 +0300

    feat(app): added circular progress
    added circular progress element

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,57 @@
-import React, { useEffect } from 'react'
+import * as React from 'react'
 
+import { CircularProgress, Box } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
+import { CustomizedSnackbars } from '../common/components/errorSnackBar/ErrorSnackBar'
 import { Header } from '../common/components/Header/Header'
 import { PATH } from '../constants/routePaths.enum'
-import { useAppSelector } from '../hooks/reduxHooks'
+import { useAppDispatch, useAppSelector } from '../hooks/reduxHooks'
 import { AppRoutes } from '../routes/routes'
 
 import s from './App.module.scss'
+import { initializeAppTC } from './appSlice'
 
 function App() {
-  const isLoggedIn = useAppSelector(state => state.isLoggedIn.isLoggedIn)
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector(state => state.isLoggedIn.isLoggedIn)
+  const [isAppLoaded, setIsAppLoaded] = React.useState(false)
 
-  useEffect(() => {
+  const navigateToLogin = () => {
+    setIsAppLoaded(true)
+    navigate(PATH.LOGIN)
+  }
+
+  const stopLoadingPreview = () => {
+    setIsAppLoaded(true)
+  }
+
+  React.useEffect(() => {
     if (!isLoggedIn) {
-      navigate(PATH.LOGIN)
+      dispatch(initializeAppTC(navigateToLogin, stopLoadingPreview))
     }
   }, [])
 
   return (
     <>
-      <div className={s.app}>
-        {isLoggedIn && <Header />}
-        <Header />
-        <main className={s.mainContent}>
-          <AppRoutes />
-        </main>
-      </div>
+      {isAppLoaded ? (
+        <>
+          <div className={s.app}>
+            {isLoggedIn && <Header />}
+            <main className={s.mainContent}>
+              <AppRoutes />
+            </main>
+          </div>
+          <Box position={'absolute'}>
+            <CustomizedSnackbars />
+          </Box>
+        </>
+      ) : (
+        <Box className={s.circularProgress}>
+          <CircularProgress />
+        </Box>
+      )}
     </>
   )
 }
