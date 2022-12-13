02df911155991a5e06e54b0683ec3aa356e308c9
Daniil <meskal1@rambler.ru>
2022-12-13 22:00:53 +0300

    feat(routes): added protection for private routes
    Added protection for private routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 
 import { PATH } from '../constants/routePaths.enum'
 import { Page404 } from '../features/404/Page404'
@@ -9,18 +9,27 @@ import { Recovery } from '../features/auth/Recovery/Recovery'
 import { Registration } from '../features/auth/Registration/Registration'
 import { Packs } from '../features/packs/Packs'
 import { Profile } from '../features/profile/Profile'
+import { useAppSelector } from '../hooks/reduxHooks'
 
 export const AppRoutes = () => {
+  const isLoggedIn = useAppSelector(state => state.isLoggedIn.isLoggedIn)
+
+  const PrivateRoutes = () => {
+    return isLoggedIn ? <Outlet /> : <Navigate to={PATH.LOGIN} />
+  }
+
   return (
     <>
       <Routes>
-        <Route path={PATH.PACKS} element={<Packs />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path={PATH.PROFILE} element={<Profile />} />
+          <Route path={PATH.PACKS} element={<Packs />} />
+        </Route>
         <Route path={PATH.LOGIN} element={<LogInApp />} />
         <Route path={PATH.RECOVERY} element={<Recovery />} />
         <Route path={PATH.REGISTRATION} element={<Registration />} />
         <Route path={PATH.CHECK_EMAIL} element={<CheckEmail />} />
         <Route path={PATH.NEW_PASSWORD} element={<NewPassword />} />
-        <Route path={PATH.PROFILE} element={<Profile />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </>
