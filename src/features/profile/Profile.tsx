3482e73a1c2ed170b85082875ce5f2c1f3dcd57d
WladyslawK <vladkurr7890@gmail.com>
2022-12-13 23:55:09 +0100

    fix(profile) Layout corrections

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,23 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { BorderColor, CameraAlt, KeyboardBackspace } from '@mui/icons-material'
 
 import { ButtonStyled } from '../../common/components/ButtonStyled/ButtonStyled'
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
+import { authAPI } from '../../services/authApi'
 
 import s from './Profile.module.scss'
+import { authMe, InitialProfileType } from './profileReducer'
 
 type ProfileType = {}
 
 export const Profile: React.FC<ProfileType> = ({}) => {
+  const dispatch = useAppDispatch()
+
+  const profile = useAppSelector<InitialProfileType>(state => state.profile)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [avatar, setAvatar] = useState('')
+
+  useEffect(() => {
+    dispatch(authMe())
+  }, [])
   const onLogOutHandler = () => alert('Log Out')
+  const addPhotoHandler = () => alert('add photo')
+  const editNameHandler = () => alert('edit name')
+  const backToPacsHandler = () => alert('bac to packs')
+
+  console.log(profile)
 
   return (
-    <div className={s.ProfileContainer}>
-      <h1 className={s.ProfileContainer__title}>Personal Information</h1>
-      <div className={s.ProfileContainer__image}>
-        <img src="" alt="avatar" />
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      <div className={s.backToPacks}>
+        <div className={s.ProfileContainer__arrow}>
+          <KeyboardBackspace />
+        </div>
+        <div className={s.backToPacks__PacksLink} onClick={backToPacsHandler}>
+          Back to Pacs List
+        </div>
       </div>
-      <h2 className={s.ProfileContainer__name}>Ivan</h2>
-      <p className={s.ProfileContainer__email}>j&jonson@gmail.com</p>
-      <ButtonStyled name={'Log Out'} onClick={onLogOutHandler} color={'white'} />
+      <div className={s.ProfileBox}>
+        <div className={s.ProfileContainer}>
+          <h1 className={s.ProfileContainer__title}>Personal Information</h1>
+          <div className={s.ProfileContainer__image}>
+            <img src={avatar} alt="avatar" />
+            <div className={s.ProfileContainer__addPhoto} onClick={addPhotoHandler}>
+              <CameraAlt />
+            </div>
+          </div>
+          <div className={s.nameContainer}>
+            <div className={s.ProfileContainer__name}>
+              <h2>{profile.name}</h2>
+              <span className={s.ProfileContainer__edit} onClick={editNameHandler}>
+                <BorderColor />
+              </span>
+            </div>
+          </div>
+          <p className={s.ProfileContainer__email}>{profile.email}</p>
+          <ButtonStyled name={'Log Out'} onClick={onLogOutHandler} color={'white'} />
+        </div>
+      </div>
     </div>
   )
 }
