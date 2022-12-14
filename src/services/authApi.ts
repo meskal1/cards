da312a3077273f35831953208e53ff7c6d34b71d
Daniil <meskal1@rambler.ru>
2022-12-14 21:38:12 +0300

    build(src): test environment

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 
 // API
 const instance = axios.create({
-  baseURL: 'https://123.ru',
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 })
 
