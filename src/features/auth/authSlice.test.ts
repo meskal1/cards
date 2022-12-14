a1899e35aaa0f4b7c008b57a655f28c91f7ca707
sanyalu <sanyalu97@gmail.com>
2022-12-14 23:45:59 +0100

    test(auth): add authSlice unit test (part 2)

--- src/features/auth/authSlice.test.ts
+++ src/features/auth/authSlice.test.ts
@@ -1,18 +1,36 @@
 import { RequestStatusType } from '../../app/appSlice'
 
-import { authReducer, AuthStateType } from './authSlice'
+import { authReducer, AuthStateType, setAuthStatus, setIsLoggedInAC } from './authSlice'
 
-let initialState: AuthStateType
+describe('auth reducer tests', () => {
+  let initialState: AuthStateType
 
-beforeEach(() => {
-  initialState = {
-    isLoggedIn: false,
-    status: 'idle' as RequestStatusType,
-  }
-})
+  beforeEach(() => {
+    initialState = {
+      isLoggedIn: false,
+      status: 'idle' as RequestStatusType,
+    }
+  })
 
-describe('Auth reducers', () => {
   test('should return the initial state', () => {
     expect(authReducer(initialState, { type: undefined })).toBe(initialState)
+  })
+
+  test('should set isLoggedIn to true', () => {
+    expect(authReducer(initialState, setIsLoggedInAC({ isLoggedIn: true })).isLoggedIn).toBeTruthy()
+  })
+
+  test('should set isLoggedIn to false', () => {
+    initialState.isLoggedIn = true
+    expect(authReducer(initialState, setIsLoggedInAC({ isLoggedIn: false })).isLoggedIn).toBeFalsy()
+  })
+
+  test('should set status to loading', () => {
+    expect(authReducer(initialState, setAuthStatus({ status: 'loading' })).status).toBe('loading')
+  })
+
+  test('should set status to idle', () => {
+    initialState.status = 'loading'
+    expect(authReducer(initialState, setAuthStatus({ status: 'idle' })).status).toBe('idle')
   })
 })
