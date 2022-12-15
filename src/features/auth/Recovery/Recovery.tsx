d11d4c315799bbf0bdef8033be269b7bb24315a1
sanyalu <sanyalu97@gmail.com>
2022-12-15 21:17:31 +0100

    feat(src): add formik+validation to Recovery

--- src/features/auth/Recovery/Recovery.tsx
+++ src/features/auth/Recovery/Recovery.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 
+import { useFormik } from 'formik'
 import { Link } from 'react-router-dom'
 
 import { CustomButton } from '../../../common/components/CustomButton/CustomButton'
 import { CustomInput } from '../../../common/components/CustomInput/CustomInput'
 import { PATH } from '../../../constants/routePaths.enum'
+import { validationSchemaForgotPassword } from '../../../utils/validationSchema'
 
 import s from './Recovery.module.scss'
 
 type RecoveryType = {}
 
 export const Recovery: React.FC<RecoveryType> = ({}) => {
+  const { handleSubmit, getFieldProps, errors, touched } = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: validationSchemaForgotPassword,
+    onSubmit: values => {
+      alert(JSON.stringify(values))
+    },
+  })
+
   return (
     <div className={s.forgotPassword}>
       <form className={s.form} onSubmit={handleSubmit}>
