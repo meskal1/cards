6d20392646ace11c37376c83c131b460e238a200
sanyalu <sanyalu97@gmail.com>
2022-12-15 21:02:25 +0100

    feat(auth): create Recovery layout

--- src/features/auth/Recovery/Recovery.tsx
+++ src/features/auth/Recovery/Recovery.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import { CustomButton } from '../../../common/components/CustomButton/CustomButton'
+import { CustomInput } from '../../../common/components/CustomInput/CustomInput'
+import { PATH } from '../../../constants/routePaths.enum'
+
+import s from './Recovery.module.scss'
+
 type RecoveryType = {}
 
 export const Recovery: React.FC<RecoveryType> = ({}) => {
   return (
-    <>
-      <div>Recovery</div>
-    </>
+    <div className={s.forgotPassword}>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <h3 className={s.title}>Forgot your password?</h3>
+        <CustomInput
+          className={s.input}
+          label={'email'}
+          error={!!errors.email && touched.email}
+          helperText={touched.email && errors.email}
+          {...getFieldProps('email')}
+        />
+        <p className={s.help}>Enter your email address and we will send you further instructions</p>
+        <CustomButton className={s.button} fullWidth>
+          Send instructions
+        </CustomButton>
+        <p className={s.forgotQuestion}>Have you remembered your password?</p>
+        <Link className={s.forgotLink} to={PATH.LOGIN}>
+          Try logging in
+        </Link>
+      </form>
+    </div>
   )
 }
