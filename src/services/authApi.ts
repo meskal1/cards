472dd4426326b61d1a905e18fbcae2f696e42c09
sanyalu <sanyalu97@gmail.com>
2022-12-16 15:17:00 +0100

    fix(services): fix baseUrl in onlyProdInstance

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -9,7 +9,7 @@ const instance = axios.create({
 })
 
 const onlyProdInstance = axios.create({
-  baseURL: 'https://neko-back.herokuapp.com/2.0',
+  baseURL: 'https://neko-back.herokuapp.com/2.0/',
   withCredentials: true,
 })
 
