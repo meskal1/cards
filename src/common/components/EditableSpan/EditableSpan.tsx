171c85e417a04088fa18b660a02c841d1bc5b0e8
WladyslawK <vladkurr7890@gmail.com>
2022-12-17 20:12:16 +0100

    fix (editableSpan) editableSpan corrections

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -2,28 +2,59 @@ import * as React from 'react'
 
 import { BorderColor, Send } from '@mui/icons-material'
 
+import { setAppAlertMessage } from '../../../app/appSlice'
+import { useAppDispatch } from '../../../hooks/reduxHooks'
+import { CustomInput } from '../CustomInput/CustomInput'
+
+import s from './EditableSpan.module.scss'
+
 type EditableSpanType = {
   name: string
   changeProfile: (name: string) => void
 }
 
 export const EditableSpan: React.FC<EditableSpanType> = ({ name, changeProfile }) => {
-  const [title, setName] = React.useState(name)
+  const [title, setTitle] = React.useState(name)
   const [edit, setEdit] = React.useState(false)
+
+  const dispatch = useAppDispatch()
 
   const changeEditHandler = () => {
     if (edit) {
-      //changeProfile()
+      if (!title) {
+        dispatch(setAppAlertMessage({ messageType: 'error', messageText: 'name is required! ' }))
+
+        return
+      }
+      changeProfile(title)
     }
     setEdit(!edit)
   }
+  const changeTitleHandler = (newTitle: string) => setTitle(newTitle)
 
   return (
-    <>
-      {edit ? <input type="text" name="" id="" /> : <span>{name}</span>}
-      <span>
-        {edit ? <Send onClick={changeEditHandler} /> : <BorderColor onClick={changeEditHandler} />}
-      </span>
-    </>
+    <div className={s.Container} onBlur={changeEditHandler}>
+      {edit ? (
+        <div className={s.NameContainer}>
+          <div className={s.InputContainer}>
+            <CustomInput
+              value={title}
+              onChange={event => changeTitleHandler(event.currentTarget.value)}
+              className={s.InputField}
+              autoFocus={true}
+            />
+          </div>
+
+          <div className={s.SendMarker}>
+            <Send className={s.Marker} />
+          </div>
+        </div>
+      ) : (
+        <p>
+          <span className={s.InputContainer}>{name}</span>
+          <BorderColor className={s.Marker} onClick={changeEditHandler} />
+        </p>
+      )}
+    </div>
   )
 }
