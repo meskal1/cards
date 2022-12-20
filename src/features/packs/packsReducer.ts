b594635aefeed3587357f377464c2e39559db72c
WladyslawK <vladkurr7890@gmail.com>
2022-12-20 14:21:25 +0100

    fead (Table) packsAPI vs pcks-reducer

--- src/features/packs/packsReducer.ts
+++ src/features/packs/packsReducer.ts
@@ -1,1 +1,18 @@
 import React from 'react'
+
+const initialState: InitialStateType = {}
+
+export const packsReducer = (state: InitialStateType = initialState, action: ActionType) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+//actions
+
+//thunks
+
+//types
+type InitialStateType = {}
+type ActionType = { type: string }
