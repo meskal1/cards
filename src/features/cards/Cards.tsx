37a7a4f3dfafabd79fa5fca201acc6fd230c5281
Daniil <meskal1@rambler.ru>
2022-12-22 21:56:18 +0300

    feat(cards): added cards page and slice for it

---
+++ src/features/cards/Cards.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+
+import { CustomPagination } from '../../common/components/CustomPagination/CustomPagination'
+import { CustomSearch } from '../../common/components/CustomSearch/CustomSearch'
+import { PageTitleBlock } from '../../common/components/PageTitleBlock/PageTitleBlock'
+import { useAppDispatch } from '../../hooks/reduxHooks'
+
+import s from './Cards.module.scss'
+import { getCardsTC } from './cardsSlice'
+
+type CardsType = {}
+
+export const Cards: React.FC<CardsType> = React.memo(({}) => {
+  const dispatch = useAppDispatch()
+
+  const handleTitleButton = () => {
+    // dispatch() Add new card
+  }
+
+  React.useEffect(() => {
+    dispatch(getCardsTC())
+  }, [])
+
+  return (
+    <>
+      <div className={s.cardsContainer}>
+        <div className={s.cards__controlBlock}>
+          <PageTitleBlock
+            linkToPacks
+            title={'packs list'}
+            button={'add new pack'}
+            buttonClick={handleTitleButton}
+          />
+          <div className={s.cards__controlPanel}>
+            <CustomSearch cards />
+          </div>
+        </div>
+        <div className={s.cards__table}>packs table</div>
+        <CustomPagination cards />
+      </div>
+    </>
+  )
+})
