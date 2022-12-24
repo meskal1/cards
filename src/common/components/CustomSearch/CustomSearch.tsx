d9f5a4b0c0e84821e7631c3695ff4d67016b304d
Daniil <meskal1@rambler.ru>
2022-12-24 17:49:51 +0300

    fix(caustom-search): fix logic caustom-search component

--- src/common/components/CustomSearch/CustomSearch.tsx
+++ src/common/components/CustomSearch/CustomSearch.tsx
@@ -1,59 +1,65 @@
 import * as React from 'react'
 
 import { TextField } from '@mui/material'
-import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom'
+import { createSearchParams, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 
+import { PATH } from '../../../constants/routePaths.enum'
 import { getCardsTC, setSearchCards } from '../../../features/cards/cardsSlice'
 import { getPacksTC, setSearchPacks } from '../../../features/packs/packsSlice'
 import { useAppDispatch } from '../../../hooks/reduxHooks'
 import { useDebounce } from '../../../hooks/useDebounce'
-import { getSearchParams } from '../../../utils/getAllSearchParams'
+import { getSearchParams } from '../../../utils/getSearchParams'
 
 import s from './CustomSearch.module.scss'
 
 type CustomSearchType = {
   cards?: boolean
-  customSearchParams: (param: string, value: string) => void
 }
 
-export const CustomSearch: React.FC<CustomSearchType> = React.memo(
-  ({ cards, customSearchParams }) => {
-    const dispatch = useAppDispatch()
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [inputValue, setInputValue] = React.useState(
-      searchParams.get('search') || searchParams.get('cardQuestion') || ''
-    )
-    const debouncedValue = useDebounce(inputValue)
-    const allParams = getSearchParams(searchParams)
+export const CustomSearch: React.FC<CustomSearchType> = ({ cards }) => {
+  const dispatch = useAppDispatch()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const allParams = getSearchParams(searchParams)
+  const inintInputValue = allParams.search || allParams.cardQuestion || ''
+  const [inputValue, setInputValue] = React.useState(inintInputValue)
+  const debouncedValue = useDebounce(inputValue)
 
-    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputValue(e.currentTarget.value)
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value)
+  }
+
+  React.useEffect(() => {
+    if (debouncedValue) {
+      setSearchParams({ search: debouncedValue })
     }
 
-    React.useEffect(() => {
-      if (cards) {
-        dispatch(setSearchCards({ cardQuestion: inputValue }))
-        dispatch(getCardsTC())
-      } else {
-        dispatch(setSearchPacks({ search: inputValue }))
-        dispatch(getPacksTC())
-      }
-    }, [debouncedValue])
+    if (inputValue === '') {
+      cards ? searchParams.delete('cardQuestion') : searchParams.delete('search')
+      setSearchParams(searchParams)
+    }
 
-    return (
-      <>
-        <div className={s.searchContainer}>
-          <p className={s.search__title}>search</p>
-          <TextField
-            className={s.search__input}
-            value={inputValue}
-            variant={'outlined'}
-            type={'text'}
-            autoComplete="off"
-            onChange={handleChangeInput}
-          />
-        </div>
-      </>
-    )
-  }
-)
+    if (cards) {
+      dispatch(setSearchCards({ cardQuestion: inputValue }))
+      dispatch(getCardsTC())
+    } else {
+      dispatch(setSearchPacks({ search: inputValue }))
+      dispatch(getPacksTC())
+    }
+  }, [debouncedValue])
+
+  return (
+    <>
+      <div className={s.searchContainer}>
+        <p className={s.search__title}>search</p>
+        <TextField
+          className={s.search__input}
+          value={inputValue}
+          variant={'outlined'}
+          type={'search'}
+          autoComplete="off"
+          onChange={handleChangeInput}
+        />
+      </div>
+    </>
+  )
+}
