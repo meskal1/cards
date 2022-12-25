3d1776a2d486d42bafea8f1808d8ea77791c1311
Daniil <meskal1@rambler.ru>
2022-12-25 21:01:40 +0300

    fix(table data): remove unused files

--- src/constants/tableData.ts
+++
@@ -1,5 +0,0 @@
-export const packsTable = ['Name', 'Cards', 'Last Updates', 'Created by', 'Actions']
-
-export const packsControlPanel = ['search', 'Show Packs cards', 'Number of Cards']
-
-export const cardsTable = []
