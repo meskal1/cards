087bbd5116a4eb12eb6143633087167b14f73b9b
Daniil <meskal1@rambler.ru>
2022-12-29 21:33:35 +0300

    fix(hooks): added useGetSearchParams hook

--- src/utils/getSearchParams.ts
+++
@@ -1,9 +0,0 @@
-export const getSearchParams = (searchParams: URLSearchParams) => {
-  const allParams = {} as { [key: string]: string }
-
-  searchParams.forEach((value: string, key: string) => {
-    allParams[key] = value
-  })
-
-  return allParams
-}
