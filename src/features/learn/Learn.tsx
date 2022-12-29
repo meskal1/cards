eb666c5cbd3d9e1fc13618fe05d52f1ed75b1ce1
WladyslawK <vladkurr7890@gmail.com>
2022-12-29 22:30:48 +0100

    fix (LearnSlice) Initial Layout vs Logic of rate

--- src/features/learn/Learn.tsx
+++ src/features/learn/Learn.tsx
@@ -1,20 +1,126 @@
 import React from 'react'
 
+import { Button, FormControl, FormLabel, Radio, RadioGroup, FormControlLabel } from '@mui/material'
+import { useFormik } from 'formik'
+import { useSelector } from 'react-redux'
+import { useSearchParams } from 'react-router-dom'
+import { number } from 'yup'
+
+import { RootStateType } from '../../app/store'
 import { BackToPacks } from '../../common/components/BackToPacks/BackToPacks'
+import { useAppDispatch } from '../../hooks/reduxHooks'
+import { ServerCardType } from '../../services/cardsApi'
+import { getSearchParams } from '../../utils/getSearchParams'
 
 import s from './Learn.module.scss'
+import { getCards } from './LearnSlice'
+
+const initialCard = {
+  _id: '',
+  cardsPack_id: '',
+  user_id: '',
+  answer: '',
+  question: '',
+  grade: 0,
+  shots: 0,
+  comments: '',
+  type: '',
+  rating: 0,
+  more_id: '',
+  created: '',
+  updated: '',
+  __v: 0,
+}
 
 export const Learn = () => {
-  React.useEffect(() => {}, [])
+  const [searchParams] = useSearchParams()
+  const allParams = getSearchParams(searchParams)
+  const dispatch = useAppDispatch()
+  const [card, setCard] = React.useState<ServerCardType>(initialCard)
+  const [showAnswer, setShowAnswer] = React.useState(false)
+  const cards = useSelector<RootStateType, ServerCardType[]>(state => state.learn.cards)
+
+  console.log('All params: ', allParams)
+  console.log('Cards: ', cards)
+
+  React.useEffect(() => {
+    dispatch(getCards({ cardsPack_id: allParams.cardsPack_id }))
+
+    if (allParams.card_id) {
+      const selectedCard = cards.find(card => card._id === allParams.card_id)
+
+      console.log('SELECTED ', selectedCard)
+      if (selectedCard) setCard(selectedCard)
+    }
+  }, [])
+
+  console.log('Card question: ', card.question)
+
+  const handleShowAnswer = () => setShowAnswer(!showAnswer)
+
+  const formik = useFormik({
+    initialValues: {
+      grade: '',
+    },
+    validate: values => {
+      const errors: formikErrorsType = {}
+
+      if (!values.grade) {
+        errors.grade = 'rate field is required'
+      }
+    },
+    onSubmit: values => {
+      console.log(values)
+    },
+  })
 
   return (
     <>
       <div className={s.mainContainer}>
         <BackToPacks />
         <div className={s.learnContainer}>
-          <p>Question</p>
+          <p>
+            <b>Question:</b> {card.question}
+          </p>
+          {showAnswer ? (
+            <div>
+              <p>
+                <b>Answer:</b> {card.answer}
+              </p>
+              <form onSubmit={formik.handleSubmit}>
+                <FormControl>
+                  <FormLabel id="demo-radio-buttons-group-label">Rate yourself</FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    {...formik.getFieldProps('grade')}
+                  >
+                    <FormControlLabel value="1" control={<Radio />} label="Did not know" />
+                    <FormControlLabel value="2" control={<Radio />} label="Forgot" />
+                    <FormControlLabel value="3" control={<Radio />} label="A lot of thought" />
+                    <FormControlLabel value="4" control={<Radio />} label="Confused" />
+                    <FormControlLabel value="5" control={<Radio />} label="Knew the answer" />
+                  </RadioGroup>
+                </FormControl>
+                <Button
+                  type={'submit'}
+                  variant={'contained'}
+                  disabled={formik.errors.grade ? true : false}
+                >
+                  Next
+                </Button>
+              </form>
+            </div>
+          ) : (
+            <Button type={'button'} variant={'contained'} onClick={handleShowAnswer}>
+              Show Answer
+            </Button>
+          )}
         </div>
       </div>
     </>
   )
+}
+
+type formikErrorsType = {
+  grade?: string
 }
