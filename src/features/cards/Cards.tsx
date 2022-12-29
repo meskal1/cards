e82ed7ab78b04253a227a73a52bce2b2b5d0a877
Daniil <meskal1@rambler.ru>
2022-12-29 21:20:50 +0300

    fix(cards): added some logic for better usability

--- src/features/cards/Cards.tsx
+++ src/features/cards/Cards.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { Navigate, useSearchParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { CustomButton } from '../../common/components/CustomButton/CustomButton'
 import { CustomPagination } from '../../common/components/CustomPagination/CustomPagination'
@@ -8,79 +8,97 @@ import { CustomSearch } from '../../common/components/CustomSearch/CustomSearch'
 import { PageTitleBlock } from '../../common/components/PageTitleBlock/PageTitleBlock'
 import { PATH } from '../../constants/routePaths.enum'
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
-import { getSearchParams } from '../../utils/getSearchParams'
+import { useGetSearchParams } from '../../hooks/useGetSearchParams'
 
 import s from './Cards.module.scss'
 import {
   AppCardType,
   CardsErrorType,
   clearCardsState,
+  setError,
   updateCardsQueryParamsTC,
 } from './cardsSlice'
 import { CardsTable } from './CardsTable/CardsTable'
 
 export const Cards = () => {
   const dispatch = useAppDispatch()
-  const [searchParams] = useSearchParams()
-  const allParams = getSearchParams(searchParams)
+  const allParams = useGetSearchParams()
+  const [showChildren, setShowChildren] = React.useState(false)
   const tableData = useAppSelector<AppCardType[]>(state => state.cards.tableData)
+  const cardsPack_id = useAppSelector(state => state.cards.queryParams.cardsPack_id)
   const packName = useAppSelector(state => state.cards.cardsData.packName)
   const packUserId = useAppSelector(state => state.cards.cardsData.packUserId)
   const myId = useAppSelector(state => state.profile.userData.id)
   const cardsError = useAppSelector<CardsErrorType>(state => state.cards.error)
   const isTableEmpty = !!tableData.length
   const isItMyPack = packUserId === myId
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const titleButtonName =
+    isTableEmpty || allParams.cardQuestion ? `${isItMyPack ? 'add new card' : 'learn to pack'}` : ''
 
   const handleTitleButton = React.useCallback(() => {
     // dispatch() Add new card
   }, [])
 
   React.useEffect(() => {
-    dispatch(updateCardsQueryParamsTC(allParams))
+    if (cardsPack_id === '' || cardsPack_id !== id) {
+      ;(async () => {
+        await dispatch(updateCardsQueryParamsTC({ ...allParams, cardsPack_id: id }))
+        setShowChildren(true)
+      })()
+    }
+  }, [id])
 
+  React.useEffect(() => {
+    if (cardsError === 'WRONG_ID') {
+      navigate(PATH.PAGE_NOT_FOUND, { replace: true })
+      dispatch(setError({ error: null }))
+    }
+  }, [cardsError])
+
+  React.useEffect(() => {
     return () => {
       dispatch(clearCardsState())
     }
-  }, [searchParams])
-
-  if (cardsError === 'WRONG_ID') {
-    return <Navigate to={PATH.PAGE_NOT_FOUND} replace />
-  }
+  }, [])
 
   return (
     <>
-      <div className={s.cardsContainer}>
-        <div className={s.cards__controlBlock}>
-          <PageTitleBlock
-            linkToPacks
-            title={packName}
-            button={isTableEmpty ? `${isItMyPack ? 'add new card' : 'learn to pack'}` : ''}
-            buttonClick={handleTitleButton}
-          />
-          {isTableEmpty && (
-            <div className={s.cards__controlPanel}>
-              <CustomSearch cards />
+      {showChildren && (
+        <div className={s.cardsContainer}>
+          <div className={s.cards__controlBlock}>
+            <PageTitleBlock
+              linkToPacks
+              title={packName}
+              button={titleButtonName}
+              buttonClick={handleTitleButton}
+            />
+            {(isTableEmpty || allParams.cardQuestion) && (
+              <div className={s.cards__controlPanel}>
+                <CustomSearch cards />
+              </div>
+            )}
+          </div>
+          {isTableEmpty ? (
+            <>
+              <CardsTable isMine={isItMyPack} />
+              <CustomPagination cards />
+            </>
+          ) : (
+            <div className={s.cards__emptyBlock}>
+              <h3 className={s.cards__emptyTitle}>
+                {`no cards found. ${isItMyPack ? 'Click add new card to fill this pack' : ''}`}
+              </h3>
+              {isItMyPack && (
+                <CustomButton onClick={handleTitleButton}>
+                  <p>add new card</p>
+                </CustomButton>
+              )}
             </div>
           )}
         </div>
-        {isTableEmpty ? (
-          <>
-            <CardsTable isMine={isItMyPack} />
-            <CustomPagination cards />
-          </>
-        ) : (
-          <div className={s.cards__emptyBlock}>
-            <h3 className={s.cards__emptyTitle}>
-              {`this pack is empty. ${isItMyPack ? 'Click add new card to fill this pack' : ''}`}
-            </h3>
-            {isItMyPack && (
-              <CustomButton onClick={handleTitleButton}>
-                <p>add new card</p>
-              </CustomButton>
-            )}
-          </div>
-        )}
-      </div>
+      )}
     </>
   )
 }
