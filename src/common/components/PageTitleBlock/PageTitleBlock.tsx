f4d69df015e429ad070280ef5d70b89a4c469de3
Daniil <meskal1@rambler.ru>
2023-01-04 16:12:06 +0300

    feat(components): added actions menu

--- src/common/components/PageTitleBlock/PageTitleBlock.tsx
+++ src/common/components/PageTitleBlock/PageTitleBlock.tsx
@@ -1,9 +1,11 @@
-import { FC, memo } from 'react'
+import { FC, memo, useState } from 'react'
 
-import { AddPack } from '../../../features/packs/Modals/AddPack/AddPack'
+import { Portal } from '@mui/base'
+
+import { EditPack } from '../../../features/packs/Modals/EditPack/EditPack'
+import { useAppSelector } from '../../../hooks/reduxHooks'
 import { BackToPacks } from '../BackToPacks/BackToPacks'
 import { CustomButton } from '../CustomButton/CustomButton'
-import { CustomModalDialog } from '../ModalDialog/CustomModalDialog'
 
 import s from './PageTitleBlock.module.scss'
 
@@ -16,23 +18,77 @@ type PageTitleBlockType = {
 
 export const PageTitleBlock: FC<PageTitleBlockType> = memo(
   ({ linkToPacks, button, title, buttonClick }) => {
-    const handleClick = () => {
+    const packUserId = useAppSelector(state => state.cards.cardsData.packUserId)
+    const packId = useAppSelector(state => state.cards.tableData[0]?.cardsPack_id)
+    const myId = useAppSelector(state => state.profile.userData.id)
+    const isItMyPack = packUserId === myId
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const menuSheet = isMenuOpen ? s.pageTitleBlock__menuSheet : ''
+    const menuItemStyle = isMenuOpen ? s.pageTitleBlock__menuItemStyle : ''
+    const [openModals, setOpenModals] = useState<number[]>([0, 0, 0])
+    const showModals = openModals.reduce((a, b) => a + b, 0)
+    const dataEdit = { id: packId, name: title }
+    const portalStyle = isMenuOpen ? s.portal : ''
+    const portalStyleBg = showModals ? s.portalBgColor : ''
+
+    const handleClickButton = () => {
       buttonClick()
+    }
+
+    const handleToogleMenu = () => {
+      setIsMenuOpen(!isMenuOpen)
+    }
+
+    const handleCloseMenu = () => {
+      setIsMenuOpen(false)
+      setOpenModals([0, 0, 0])
+    }
+
+    const handleLearn = () => {
+      setOpenModals([1, 0, 0])
+    }
+
+    const handleEdit = () => {
+      setOpenModals([0, 1, 0])
+    }
+
+    const handleDelete = () => {
+      setOpenModals([0, 0, 1])
     }
 
     return (
       <>
+        <Portal>
+          <div className={portalStyle || portalStyleBg} onClick={handleCloseMenu}></div>
+        </Portal>
+
         <div className={s.pageTitleBlockContainer}>
           {linkToPacks && <BackToPacks />}
           <div className={s.pageTitleBlock__titleBlock}>
             <h2 className={s.pageTitleBlock__title}>{title}</h2>
+            {isItMyPack && (
+              <div className={`${s.pageTitleBlock__menuContainer} ${menuSheet}`}>
+                <ul className={s.pageTitleBlock__menu} onClick={handleToogleMenu}>
+                  <li className={`${s.menuItem} ${menuItemStyle}`} onClick={handleLearn}></li>
+                  <li className={`${s.menuItem} ${menuItemStyle}`} onClick={handleEdit}></li>
+                  <li className={`${s.menuItem} ${menuItemStyle}`} onClick={handleDelete}></li>
+                </ul>
+              </div>
+            )}
+
             {button && (
-              <CustomButton onClick={handleClick}>
+              <CustomButton onClick={handleClickButton}>
                 <p>{button}</p>
               </CustomButton>
             )}
           </div>
         </div>
+
+        {isItMyPack && !!showModals && (
+          <div className={showModals ? s.modalsContainer : ''}>
+            {!!openModals[1] && <EditPack data={dataEdit} closeModal={handleCloseMenu} />}
+          </div>
+        )}
       </>
     )
   }
