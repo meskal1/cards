abd782c4c245278f5307fc99c42e9ae51b7c0cf8
Daniil <meskal1@rambler.ru>
2023-01-12 03:18:09 +0300

    fix(components): fix unnecessary renders

--- src/common/components/CustomSearch/CustomSearch.tsx
+++ src/common/components/CustomSearch/CustomSearch.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect, FC, ChangeEvent } from 'react'
+import { useState, FC, ChangeEvent } from 'react'
 
 import { TextField } from '@mui/material'
-import { useLocation, useSearchParams } from 'react-router-dom'
 
 import { setCardsQueryParams } from '../../../features/cards/cardsSlice'
 import { setPacksQueryParams } from '../../../features/packs/packsSlice'
 import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks'
 import { useDebounce } from '../../../hooks/useDebounce'
-import { useGetSearchParams } from '../../../hooks/useGetSearchParams'
+import { useEffectAfterMount } from '../../../hooks/useEffectAfterMount'
+import { useGetQueryParams } from '../../../hooks/useGetQueryParams'
 
 import s from './CustomSearch.module.scss'
 
@@ -17,47 +17,36 @@ type CustomSearchType = {
 
 export const CustomSearch: FC<CustomSearchType> = ({ cards }) => {
   const dispatch = useAppDispatch()
-  const location = useLocation()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const allParams = useGetSearchParams('always')
-  const initInputValue = allParams.search || allParams.cardQuestion || ''
-  const [inputValue, setInputValue] = useState(initInputValue)
-  const debouncedValue = useDebounce(inputValue)
+  const allParams = useGetQueryParams()
   const search = useAppSelector(state => state.packs.queryParams.search)
   const cardQuestion = useAppSelector(state => state.cards.queryParams.cardQuestion)
+  const [inputValue, setInputValue] = useState(allParams.search || allParams.cardQuestion || '')
+  const debouncedValue = useDebounce(inputValue)
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.currentTarget.value)
   }
 
-  useEffect(() => {
-    const isItWorthUpdating = () => {
-      if (cards) {
-        if (cardQuestion !== inputValue) {
-          dispatch(setCardsQueryParams({ cardQuestion: inputValue }))
-        }
-      } else {
-        if (search !== inputValue) {
-          dispatch(setPacksQueryParams({ search: inputValue }))
-        }
+  console.log('RENDER SEARCH')
+  useEffectAfterMount(() => {
+    if (cards) {
+      if (cardQuestion !== inputValue) {
+        dispatch(setCardsQueryParams({ cardQuestion: inputValue }))
       }
-    }
-
-    if (inputValue !== search && inputValue !== '') {
-      setSearchParams({ ...allParams, [`${cards ? 'cardQuestion' : 'search'}`]: inputValue })
-      isItWorthUpdating()
-    }
-
-    if (debouncedValue === '' && initInputValue) {
-      cards ? searchParams.delete('cardQuestion') : searchParams.delete('search')
-      setSearchParams(searchParams)
-      isItWorthUpdating()
+    } else if (search !== inputValue) {
+      dispatch(setPacksQueryParams({ search: inputValue }))
     }
   }, [debouncedValue])
 
-  useEffect(() => {
-    setInputValue(initInputValue)
-  }, [location.search])
+  useEffectAfterMount(() => {
+    setInputValue(allParams.search || allParams.cardQuestion || '')
+  }, [allParams.search, allParams.cardQuestion])
+
+  useEffectAfterMount(() => {
+    if (!search) {
+      setInputValue('')
+    }
+  }, [search])
 
   return (
     <>
