eeca0d54dd6c3ca6fb0763a6cf83c990166b2e72
Daniil <meskal1@rambler.ru>
2023-02-04 16:11:02 +0300

    fix(features): fix getPacksTC, deletePackTC, updatePackTC, some types, initialState

--- src/features/packs/packsSlice.ts
+++ src/features/packs/packsSlice.ts
@@ -3,7 +3,12 @@ import { AxiosError } from 'axios'
 
 import { RequestStatusType } from '../../app/appSlice'
 import { RootStateType } from '../../app/store'
-import { CreatePackType, packsAPI, ServerPackType } from '../../services/packsApi'
+import {
+  CreatePackType,
+  packsAPI,
+  PacksResponseType,
+  ServerPackType,
+} from '../../services/packsApi'
 import { handleServerNetworkError } from '../../utils/errorUtils'
 import { logOutTC } from '../auth/authSlice'
 import { setCardsData } from '../cards/cardsSlice'
@@ -18,16 +23,17 @@ export const initialPacksQueryParams = {
   isMyPacks: '' as 'yes' | '',
 }
 
+const intialPacksData = {
+  minCardsCount: 0,
+  maxCardsCount: 0,
+  cardPacksTotalCount: 0,
+}
+
 const initialState = {
   queryParams: initialPacksQueryParams,
-  cardsCount: {
-    minCardsCount: 0,
-    maxCardsCount: 0,
-    cardPacksTotalCount: 0,
-  },
+  packsData: intialPacksData as PacksDataPayloadType,
   tableData: [] as AppPackType[],
-  brokenImages: [] as Array<string | null>,
-  status: 'loading' as RequestStatusType,
+  status: 'idle' as RequestStatusType,
 }
 
 export const getPacksTC = createAsyncThunk(
@@ -47,13 +53,11 @@ export const getPacksTC = createAsyncThunk(
         // block,
       }
       const response = await packsAPI.getPacks(data)
-      const minCardsCount = response.data.minCardsCount
-      const maxCardsCount = response.data.maxCardsCount
-      const cardPacksTotalCount = response.data.cardPacksTotalCount
+      const { cardPacks, ...restData } = response.data
 
       return {
-        tableData: response.data.cardPacks,
-        cardsCount: { minCardsCount, maxCardsCount, cardPacksTotalCount },
+        tableData: cardPacks,
+        packsData: { ...restData },
       }
     } catch (e) {
       handleServerNetworkError(dispatch, e as Error | AxiosError)
@@ -68,11 +72,13 @@ export const deletePackTC = createAsyncThunk(
   async (id: string, { dispatch, getState, rejectWithValue }) => {
     try {
       const state = getState() as RootStateType
+      const tableLength = state.packs.tableData.length
+      const page = state.packs.queryParams.page
 
       await packsAPI.deletePack(id)
 
-      if (state.packs.tableData.length === 1 && state.packs.queryParams.page > 1) {
-        dispatch(setPacksQueryParams({ page: state.packs.queryParams.page - 1 }))
+      if (tableLength === 1 && page > 1) {
+        dispatch(setPacksQueryParams({ page: page - 1 }))
       } else {
         await dispatch(getPacksTC())
       }
@@ -100,10 +106,11 @@ export const updatePackTC = createAsyncThunk(
   'packs/updatePack',
   async (data: UpdatePackDataType, { dispatch, getState, rejectWithValue }) => {
     try {
+      const { id, ...restData } = data
       const state = getState() as RootStateType
       const updateCardsData = state.cards.cardsData
 
-      await packsAPI.updatePack({ _id: data.id, name: data.name, deckCover: data.deckCover })
+      await packsAPI.updatePack({ _id: id, ...restData })
       await dispatch(getPacksTC())
       dispatch(setCardsData({ ...updateCardsData, packName: data.name }))
     } catch (e) {
@@ -131,9 +138,6 @@ const packsSlice = createSlice({
     clearPacksQueryParams(state) {
       state.queryParams = initialState.queryParams
     },
-    setBrokenImages(state, action) {
-      state.brokenImages.push(action.payload)
-    },
   },
 
   extraReducers: builder => {
@@ -143,7 +147,7 @@ const packsSlice = createSlice({
       })
       .addCase(getPacksTC.fulfilled, (state, action) => {
         state.tableData = action.payload.tableData.map(p => ({ ...p, requestStatus: 'idle' }))
-        state.cardsCount = { ...action.payload.cardsCount }
+        state.packsData = { ...action.payload.packsData }
         state.status = 'idle'
       })
       .addCase(getPacksTC.rejected, state => {
@@ -152,9 +156,13 @@ const packsSlice = createSlice({
 
     builder
       .addCase(deletePackTC.pending, (state, action) => {
+        state.status = 'loading'
         state.tableData.forEach(p =>
           p._id === action.meta.arg ? (p.requestStatus = 'loading') : ''
         )
+      })
+      .addCase(deletePackTC.fulfilled, state => {
+        state.status = 'idle'
       })
       .addCase(deletePackTC.rejected, (state, action) => {
         state.tableData.forEach(p => (p._id === action.meta.arg ? (p.requestStatus = 'idle') : ''))
@@ -179,10 +187,11 @@ const packsSlice = createSlice({
 export const packsReducer = packsSlice.reducer
 
 // ACTIONS
-export const { setPacksQueryParams, setBrokenImages, clearPacksQueryParams } = packsSlice.actions
+export const { setPacksQueryParams, clearPacksQueryParams } = packsSlice.actions
 
 // TYPES
 export type PacksStateType = typeof initialState
+
 export type AppPackType = ServerPackType & { requestStatus: RequestStatusType }
 
 export type PacksSortValuesType =
@@ -210,10 +219,13 @@ type SetPacksQueryParamsPayloadType = {
   isMyPacks: 'yes' | ''
 }
 
+type PacksDataPayloadType = Omit<PacksResponseType, 'cardPacks'>
+
 export type PacksQueryParamsType = Partial<SetPacksQueryParamsPayloadType>
 
 export type UpdatePackDataType = {
   id: string
   name: string
-  deckCover: string
+  private: boolean
+  deckCover?: string
 }
