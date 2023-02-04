026e4830b7ee530f6d824831fc76cc31549044a4
Daniil <meskal1@rambler.ru>
2023-02-04 15:54:56 +0300

    feat(components): change sceleton for adaptive tables

--- src/common/components/CustomSkeletons/TableBodySkeleton/TableBodySkeleton.tsx
+++ src/common/components/CustomSkeletons/TableBodySkeleton/TableBodySkeleton.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react'
 
-import { Skeleton } from '@mui/material'
+import { Skeleton, useMediaQuery } from '@mui/material'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
+
+import s from '../../../../features/packs/PacksTable/PacksTableBody/PacksTableBody.module.scss'
+import { useLocationNoUpdates } from '../../../../utils/routerUtils'
 
 type TableSkeletonPropsType = {
   columnsCount: number
@@ -16,16 +19,30 @@ export const TableBodySkeleton: FC<TableSkeletonPropsType> = ({
   rowsCount,
   withActions = false,
 }) => {
-  const cells = new Array(withActions ? columnsCount + 1 : columnsCount).fill('').map((_, i) => {
+  const location = useLocationNoUpdates()
+  const isCards = new RegExp('/cards').test(location.pathname)
+  const matches599 = useMediaQuery('(max-width: 599px)')
+  const colsCountCards = isCards && matches599 ? columnsCount + 1 : columnsCount
+
+  const cells = new Array(withActions ? columnsCount + 1 : colsCountCards).fill('').map((_, i) => {
     return (
-      <TableCell key={i}>
-        <Skeleton animation={'pulse'} height={35} />
+      <TableCell key={i} className={matches599 ? s.tablePacks__bodyCell : ''}>
+        <Skeleton
+          animation={'wave'}
+          height={matches599 ? 34 : 37}
+          variant={matches599 ? 'text' : 'rounded'}
+        />
       </TableCell>
     )
   })
+
   const rows = new Array(rowsCount).fill('').map((_, i) => {
-    return <TableRow key={i}>{cells}</TableRow>
+    return (
+      <TableRow key={i} className={matches599 ? s.tablePacks__bodyRow : ''}>
+        {cells}
+      </TableRow>
+    )
   })
 
-  return <TableBody>{rows}</TableBody>
+  return <TableBody className={matches599 ? s.tablePacks__body : ''}>{rows}</TableBody>
 }
