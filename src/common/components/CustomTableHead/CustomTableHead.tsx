212e8c1b03765a8ee521784f8f78b50265d3d884
Daniil <meskal1@rambler.ru>
2023-02-04 15:39:05 +0300

    refactor(components): refactor code, change some logic, fix namings, change styles

--- src/common/components/CustomTableHead/CustomTableHead.tsx
+++ src/common/components/CustomTableHead/CustomTableHead.tsx
@@ -1,89 +1,57 @@
 import { FC } from 'react'
-export type ServerOrderType = '0' | '1'
 
-export enum TableOrder {
-  desc = '0',
-  asc = '1',
-}
-
-export type TableOrderType = 'asc' | 'desc'
-
-import Box from '@mui/material/Box'
-import TableCell from '@mui/material/TableCell'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import TableSortLabel from '@mui/material/TableSortLabel'
-import { visuallyHidden } from '@mui/utils'
-
-import { CardsSortValuesType, setCardsQueryParams } from '../../../features/cards/cardsSlice'
 import { CardsOrderByType } from '../../../features/cards/CardsTable/CardsTable'
-import { setPacksQueryParams, PacksSortValuesType } from '../../../features/packs/packsSlice'
 import { PacksOrderByType } from '../../../features/packs/PacksTable/PacksTable'
-import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks'
+import { useAppSelector } from '../../../hooks/reduxHooks'
+import { SortValuesType } from '../SortTableItems/SortTableItems'
 
 import s from './CustomTableHead.module.scss'
 
-export type HeadType<T> = {
+export type TableHeadType<T> = {
   id: T
   label: string
 }
 
 type CustomTableHeadPropsType = {
-  heads: HeadType<PacksOrderByType | CardsOrderByType>[]
-  cards?: boolean
+  heads: TableHeadType<PacksOrderByType | CardsOrderByType>[]
+  forCards?: boolean
   withActions?: boolean
 }
 
 export const CustomTableHead: FC<CustomTableHeadPropsType> = ({
   heads,
-  cards = false,
+  forCards = false,
   withActions = false,
 }) => {
-  // const serverSort = useAppSelector<PacksSortValuesType>(state => state.packs.queryParams.sortPacks)
-  const serverSort = useAppSelector<PacksSortValuesType | CardsSortValuesType>(state => {
-    return cards ? state.cards.queryParams.sortCards : state.packs.queryParams.sortPacks
+  const serverSort = useAppSelector<SortValuesType>(state => {
+    return forCards ? state.cards.queryParams.sortCards : state.packs.queryParams.sortPacks
   })
-  const dispatch = useAppDispatch()
-
-  const serverOrder = serverSort.slice(0, 1) as ServerOrderType
-  const tableOrderBy = serverSort.slice(1) as PacksOrderByType | CardsOrderByType
-  const tableOrder: TableOrderType = serverOrder === TableOrder.asc ? 'asc' : 'desc'
-
-  const handleSetSort = (property: PacksOrderByType | CardsOrderByType) => {
-    const isAsc = tableOrderBy === property && tableOrder === 'asc'
-    const newOrder = isAsc ? 'desc' : 'asc'
-    const newServerOrder:
-      | PacksSortValuesType
-      | CardsSortValuesType = `${TableOrder[newOrder]}${property}`
-
-    if (cards) {
-      dispatch(setCardsQueryParams({ sortCards: newServerOrder as CardsSortValuesType }))
-    } else {
-      dispatch(setPacksQueryParams({ sortPacks: newServerOrder as PacksSortValuesType }))
-    }
-  }
+  const serverOrder = serverSort.slice(0, 1)
+  const tableOrderBy = serverSort.slice(1)
+  const arrowOrderStyle = +serverOrder ? s.sortArrowUp : s.sortArrowDown
 
   return (
-    <TableHead className={s.tableHead}>
-      <TableRow>
-        {heads.map(h => (
-          <TableCell key={h.id}>
-            <TableSortLabel
-              active={tableOrderBy === h.id}
-              direction={tableOrderBy === h.id ? tableOrder : 'asc'}
-              onClick={() => handleSetSort(h.id)}
+    <thead className={s.tablePacks__head}>
+      <tr className={s.tablePacks__headRow}>
+        {heads.map(el => {
+          return (
+            <th
+              key={el.id}
+              className={el.label === 'Grade' ? s.gradeCell : s.tablePacks__headCell}
+              scope="col"
             >
-              {h.label}
-              {tableOrderBy === h.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {tableOrder === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-        {withActions && <TableCell>Actions</TableCell>}
-      </TableRow>
-    </TableHead>
+              <div className={s.headCellContainer}>
+                <span
+                  className={`${s.headCellTitle} ${tableOrderBy === el.id ? arrowOrderStyle : ''}`}
+                >
+                  {el.label}
+                </span>
+              </div>
+            </th>
+          )
+        })}
+        <th className={withActions ? s.tablePacks__headCell : s.actionCell} scope="col"></th>
+      </tr>
+    </thead>
   )
 }
