cc36cbfe2e10136b7bea83ad84c7b2c144190f0f
Daniil <meskal1@rambler.ru>
2023-02-04 15:49:49 +0300

    refactor(features): refactor code, change some logic, fix namings, change styles

--- src/features/cards/CardsTable/CardsTableBody/CardsTableBody.tsx
+++ src/features/cards/CardsTable/CardsTableBody/CardsTableBody.tsx
@@ -1,40 +1,26 @@
 import { FC } from 'react'
 
 import Rating from '@mui/material/Rating'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableRow from '@mui/material/TableRow'
 import dayjs from 'dayjs'
+import isBase64 from 'is-base64'
 
 import { RequestStatusType } from '../../../../app/appSlice'
-import cover from '../../../../assets/img/cover.png'
-import { HeadType } from '../../../../common/components/CustomTableHead/CustomTableHead'
+import { TableHeadType } from '../../../../common/components/CustomTableHead/CustomTableHead'
 import { PATH } from '../../../../constants/routePaths.enum'
 import { useAppSelector } from '../../../../hooks/reduxHooks'
 import { useNavigateNoUpdates } from '../../../../utils/routerUtils'
-import { AppCardType, UpdateCardType } from '../../cardsSlice'
+import { CardActionsMenu } from '../../CardActionsMenu/CardActionsMenu'
+import { AppCardType } from '../../cardsSlice'
 import { CardsOrderByType } from '../CardsTable'
 
-import { CardsActionCell } from './CardsActionCell/CardsActionCell'
 import s from './CardsTableBody.module.scss'
 
 type CardsTableBodyType = {
-  heads: HeadType<CardsOrderByType>[]
+  heads: TableHeadType<CardsOrderByType>[]
   isMine: boolean
-  openEdit: (state: boolean) => void
-  setEditData: (data: UpdateCardType) => void
-  openDelete: (state: boolean) => void
-  setDeleteData: (id: string) => void
 }
 
-export const CardsTableBody: FC<CardsTableBodyType> = ({
-  heads,
-  isMine,
-  openEdit,
-  setEditData,
-  openDelete,
-  setDeleteData,
-}) => {
+export const CardsTableBody: FC<CardsTableBodyType> = ({ heads, isMine }) => {
   const tableData = useAppSelector<AppCardType[]>(state => state.cards.tableData)
   const navigate = useNavigateNoUpdates()
 
@@ -43,69 +29,60 @@ export const CardsTableBody: FC<CardsTableBodyType> = ({
     navigate(PATH.LEARN + `/${packId}`, { state: { cardId: id } })
   }
 
-  const handleEditCard = (data: UpdateCardType) => {
-    openEdit(true)
-    setEditData(data)
-  }
-  const handleDeleteCard = (id: string) => {
-    setDeleteData(id)
-    openDelete(true)
-  }
-
   return (
-    <TableBody>
+    <tbody className={s.tableCards__body}>
       {tableData.map(row => {
         return (
-          <TableRow
-            key={row._id}
-            hover={row.requestStatus === 'idle'}
-            className={s.row}
-            onClick={() => handleOpenCard(row._id, row.cardsPack_id, row.requestStatus)}
-          >
-            {heads.map(h => {
-              return (
-                <TableCell key={h.id}>
-                  {h.id === 'question' && row.questionImg && (
-                    <p>
-                      <img
-                        src={row.questionImg}
-                        alt="deckCover"
-                        className={s.image}
-                        onError={({ currentTarget }) => {
-                          currentTarget.onerror = null // prevents looping
-                          currentTarget.src = cover
-                        }}
-                      />
-                    </p>
-                  )}
+          <tr key={row._id} className={s.tableCards__bodyRow}>
+            {heads.map((h, i) => {
+              const validImg = isBase64(row.questionImg, { mimeRequired: true })
 
-                  {h.id === 'grade' ? (
-                    <Rating value={row[h.id]} readOnly />
-                  ) : (
-                    <p className={s.tableCellText}>
-                      {h.id === 'updated' ? dayjs(row[h.id]).format('DD.MM.YYYY') : row[h.id]}
-                    </p>
-                  )}
-                </TableCell>
+              return (
+                <td
+                  className={s.tableCards__bodyCell}
+                  scope="row"
+                  key={h.id}
+                  onClick={() => handleOpenCard(row._id, row.cardsPack_id, row.requestStatus)}
+                >
+                  <div className={s.tableCards__bodyCellContent}>
+                    <div className={s.tableCards__bodyCellTitle}>
+                      {i === 0 ? `${validImg ? '' : row[h.id]}` : h.label}
+                    </div>
+
+                    {h.id === 'question' && validImg && (
+                      <div className={s.cardImageWrapper}>
+                        <img src={row.questionImg} alt="deckCover" className={s.cardImage} />
+                      </div>
+                    )}
+
+                    {h.id === 'grade' ? (
+                      <Rating className={s.rating} value={row[h.id]} readOnly />
+                    ) : (
+                      <p
+                        className={`${s.cardNameAdaptive} ${
+                          h.id === 'question' && validImg ? s.cardName : ''
+                        }`}
+                      >
+                        {h.id === 'updated' ? dayjs(row[h.id]).format('DD.MM.YYYY') : row[h.id]}
+                      </p>
+                    )}
+                  </div>
+                </td>
               )
             })}
-            {isMine && (
-              <CardsActionCell
-                isAllDisabled={row.requestStatus === 'loading'}
-                onEdit={() =>
-                  handleEditCard({
-                    id: row._id,
-                    question: row.question,
-                    answer: row.answer,
-                    questionImg: row.questionImg,
-                  })
-                }
-                onDelete={() => handleDeleteCard(row._id)}
-              />
-            )}
-          </TableRow>
+            <td className={s.tableCards__bodyCell}>
+              {isMine && (
+                <CardActionsMenu
+                  cardID={row._id}
+                  answer={row.answer}
+                  question={row.question}
+                  questionImg={row.questionImg}
+                />
+              )}
+            </td>
+          </tr>
         )
       })}
-    </TableBody>
+    </tbody>
   )
 }
--- src/features/packs/PackSlider/PackSlider.tsx
+++ src/features/packs/PackSlider/PackSlider.tsx
@@ -11,8 +11,8 @@ export const PackSlider = () => {
   const dispatch = useAppDispatch()
   const min = useAppSelector(state => state.packs.queryParams.min)
   const max = useAppSelector(state => state.packs.queryParams.max)
-  const minCardsCount = useAppSelector(state => state.packs.cardsCount.minCardsCount)
-  const maxCardsCount = useAppSelector(state => state.packs.cardsCount.maxCardsCount)
+  const minCardsCount = useAppSelector(state => state.packs.packsData.minCardsCount)
+  const maxCardsCount = useAppSelector(state => state.packs.packsData.maxCardsCount)
   const [value, setValue] = useState<number[]>([min || minCardsCount, max || maxCardsCount])
 
   const handleChange = (e: Event, newValue: number | number[]) => setValue(newValue as number[])
@@ -28,23 +28,21 @@ export const PackSlider = () => {
   }, [minCardsCount, maxCardsCount, min, max])
 
   return (
-    <>
-      <div className={s.sliderContainer}>
-        <p className={s.slider__title}>number of cards</p>
-        <div className={s.slider__block}>
-          <div className={s.slider__number}>{value[0]}</div>
-          <StyledSlider
-            className={s.slider}
-            value={value}
-            onChange={handleChange}
-            disableSwap
-            min={minCardsCount}
-            max={maxCardsCount}
-            onChangeCommitted={handleMouseUp}
-          />
-          <div className={s.slider__number}>{value[1]}</div>
-        </div>
+    <div className={s.sliderContainer}>
+      <p className={s.slider__title}>Amount of cards</p>
+      <div className={s.slider__block}>
+        <div className={s.slider__number}>{value[0]}</div>
+        <StyledSlider
+          className={s.slider}
+          value={value}
+          onChange={handleChange}
+          disableSwap
+          min={minCardsCount}
+          max={maxCardsCount}
+          onChangeCommitted={handleMouseUp}
+        />
+        <div className={s.slider__number}>{value[1]}</div>
       </div>
-    </>
+    </div>
   )
 }
