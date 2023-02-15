95e04816890145b4352c41365170f48f21395a7a
Daniil <meskal1@rambler.ru>
2023-02-15 15:04:29 +0300

    fix(utils): added check for max character password and email

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,9 +1,14 @@
 import * as yup from 'yup'
 
-const email = yup.string().email('Enter a valid email').required('Email is required')
+const email = yup
+  .string()
+  .email('Enter a valid email')
+  .max(50, 'Enter a valid email')
+  .required('Email is required')
 const password = yup
   .string()
   .min(8, 'Password must contain at least 8 characters')
+  .max(30, 'Password is too long')
   .required('Enter your password')
 const confirmPassword = yup
   .string()
